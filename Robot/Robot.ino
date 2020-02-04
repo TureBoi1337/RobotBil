2d953d521c19eaca1e9cef7ec8d94b2809b39d7c
TureBoi1337 <ture.danielsson@jgy.se>
2020-02-04 14:38:47 +0100

    changeback

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -12,7 +12,7 @@
  * Här definerar ni funktioner.
  * Lägg till funktioner och namnge dom logiskt.
  */
-SoftwareSerial Serial1(6, 7);
+- SoftwareSerial Serial1(SOFTSERIAL_RX, SOFTSERIAL_TX);
 void initPins();
 void blinkStatusLed(int del);
 void connectToWiFi(void);
